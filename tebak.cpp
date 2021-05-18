1d51ad20a1f0452d6963dc6d25d43a8ea294a569
aniisahms <83533036+aniisahms@users.noreply.github.com>
2021-05-18 22:12:03 +0700

    Update tebak.cpp

--- tebak.cpp
+++ tebak.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
 #include "question.h"
 
-void menu();
-char *jawab();
+void menu(){
+	cout << "Pilih menu:" << endl;
+	cout << "\t 1. Mulai kuis" << endl;
+	cout << "\t 2. Lihat nilai" << endl;
+	cout << "\t 3. Keluar" << endl;
+}
+
+char *jawab(){
+	char jawaban [51];
+	
+	cout << "Tulis jawaban singkat satu kata menggunakan huruf kecil: ";
+	cin.getline (jawaban, 51);
+	
+	return jawaban;
+}
+
 bool banding();
 void nilai();
 
