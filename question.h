d4a0d0349b8ae9b4d4e022cf94b1909f151301e5
2017051079_AullyaHananWulandari <83333841+aullyahananwulandari2079@users.noreply.github.com>
2021-05-18 21:46:55 +0700

    Update question.h

--- question.h
+++ question.h
@@ -2,7 +2,34 @@
 
 
 
-char tanya[26][101] = {"ini pertanyaan 1",
-                       "ini nanya"};
+char tanya[26][101] = {"1. Tombol di keyboard yang berfungsi menghapus karakter sebelah kanan kursor yaitu",
+                       "2. Sistem operasi yang hanya dapat dijalankan pada komputer jenis apple", 
+                       "3. Microsoft Windows merupakan sistem operasi komputer yang berbasis",
+                       "4. Variasi dari sistem operasi linux biasa disebut dengan", 
+                       "5. Bilangan biner merupakan sistem bilangan yang mempunyai radik paling kecil",
+                       "6. Dalam Kode BCD menggunakan kode biner sebanyak berapa bit",
+                       "7. Komponen fisik yang membentuk sistem komputer adalah",
+                       "8. Komponen non fisik untuk menjalankan, mengendalikan dan mengatur proses oleh perangkat keras komputer adalah",
+                       "9. Manusia dengan tenaga dan ilmu pengetahuan yang digunakan untuk mengoperasikan serta mengatur system komputer adalah",
+                       "10. Hardware yang berfungsi sebagai alat penunjuk untuk mengatur posisi kursor di layar adalah",
+                       "11. Microsoft word adalah contoh program aplikasi pengolah",
+                       "12. Jenis memori yang digunakan pada sistem yang menggunakan Pentium, disebut",
+                       "13. Yang berfungsi untuk membentuk fungsi pengolahan data komputer adalah",
+                       "14. Untuk melakukan cold booting dilakukan dengan menekan tombol",
+                       "15. Unit pemrosesan pada komputer",
+                       "16. Program aplikasi yang digunakan secara bebas dan boleh disebarluaskan tanpa harus membayar lisensinya disebut",
+                       "17. Suatu Jenis Jaringan Komputer dengan mencakup wilayah lokal",
+                       "18. Jenis Jaringan Komputer yang menggunakan media transmisi data tanpa menggunakan kabel",
+                       "19. Data yang dibangkitkan dari sister sumber tidak ditransmisikan secara langsung dalam bentuk aslinya",
+                       "20. Area memori yang menyimpan data ketika mereka sedang dipindahkan antara dua device atau antara device dan aplikasi",
+                       "21. Pengembangan dari sistem operasi unix yang dikembangkan oleh Linus Benedict Torvals merupakan contoh jenis sistem operasi",
+                       "22. Sistem Operasi untuk jaringan komputer dan banyak digunakan pada komputer besar seperti supercomputer merupakan contoh jenis sistem operasi",
+                       "23. Orang yang mengoperasikan komputer disebut juga dengan istilah",
+                       "24. Perangkat input yang digunakan seseorang untuk berkomunikasi dengan sistem komputer disebut",
+                       "25. Tombol di keyboard yang berfungsi menghapus karakter sebelah kiri kursor yaitu"};
 
-char jawab[26][51] = {"jawab1", "jawab2"}; 
+char jawab[26][51] = {"delete", "Macintosh","GUI","distro","2",
+                      "4","hardware","software","brainware","Mouse",
+                      "Kata","edoram","ALU","Power","CPU",
+                      "freeware","LAN","wireless","transmitter","buffering",
+                      "linux","unix","brainware","mouse","backspace"}; 
