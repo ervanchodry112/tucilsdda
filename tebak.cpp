021f349107894b33746205bcdddb5d32dc957496
Ervan Chodry <58065502+ervanchodry112@users.noreply.github.com>
2021-05-23 10:15:03 +0700

    Question.h dimasukin ke tebak.cpp

--- tebak.cpp
+++ tebak.cpp
@@ -1,5 +1,40 @@
-#include "question.h"
+#include <iostream>
+#include <stdlib.h>
+#include <cstring>
+#include <time.h>
 using namespace std;
+
+char tanya[26][201] = {"Tombol di keyboard yang berfungsi menghapus karakter sebelah kanan kursor yaitu",
+                       "Sistem operasi yang hanya dapat dijalankan pada komputer jenis apple", 
+                       "Microsoft Windows merupakan sistem operasi komputer yang berbasis",
+                       "Variasi dari sistem operasi linux biasa disebut dengan", 
+                       "Bilangan biner merupakan sistem bilangan yang mempunyai radik paling kecil",
+                       "Dalam Kode BCD menggunakan kode biner sebanyak berapa bit",
+                       "Komponen fisik yang membentuk sistem komputer adalah",
+                       "Komponen non fisik untuk menjalankan, mengendalikan dan mengatur proses oleh perangkat keras komputer adalah",
+                       "Manusia dengan tenaga dan ilmu pengetahuan yang digunakan untuk mengoperasikan serta mengatur system komputer adalah",
+                       "Hardware yang berfungsi sebagai alat penunjuk untuk mengatur posisi kursor di layar adalah",
+                       "Microsoft word adalah contoh program aplikasi pengolah",
+                       "Jenis memori yang digunakan pada sistem yang menggunakan Pentium, disebut",
+                       "Yang berfungsi untuk membentuk fungsi pengolahan data komputer adalah",
+                       "Untuk melakukan cold booting dilakukan dengan menekan tombol",
+                       "Unit pemrosesan pada komputer",
+                       "Program aplikasi yang digunakan secara bebas dan boleh disebarluaskan tanpa harus membayar lisensinya disebut",
+                       "Suatu Jenis Jaringan Komputer dengan mencakup wilayah lokal",
+                       "Jenis Jaringan Komputer yang menggunakan media transmisi data tanpa menggunakan kabel",
+                       "Data yang dibangkitkan dari sister sumber tidak ditransmisikan secara langsung dalam bentuk aslinya",
+                       "Area memori yang menyimpan data ketika mereka sedang dipindahkan antara dua device atau antara device dan aplikasi",
+                       "Pengembangan dari sistem operasi unix yang dikembangkan oleh Linus Benedict Torvals merupakan contoh jenis sistem operasi",
+                       "Sistem Operasi untuk jaringan komputer dan banyak digunakan pada komputer besar seperti supercomputer merupakan contoh jenis sistem operasi",
+                       "Orang yang mengoperasikan komputer disebut juga dengan istilah",
+                       "Perangkat input yang digunakan seseorang untuk berkomunikasi dengan sistem komputer disebut",
+                       "Tombol di keyboard yang berfungsi menghapus karakter sebelah kiri kursor yaitu"};
+
+char jawab[26][51] = {"delete", "macintosh","gui","distro","2",
+                      "4","hardware","software","brainware","mouse",
+                      "Kata","edoram","alu","power","cpu",
+                      "freeware","lan","wireless","transmitter","buffering",
+                      "linux","unix","brainware","mouse","backspace"}; 
 
 void menu();
 bool banding(char *ans, int value);
